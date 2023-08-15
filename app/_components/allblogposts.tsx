b6b870a597dc7eed86be42e150070d902794248f
Darren <darren.emory+1@gmail.com>
2023-08-15 11:30:56 -0600

    styling all blog posts section

---
+++ app/_components/allblogposts.tsx
@@ -0,0 +1,400 @@
+import Image from "next/image";
+
+import thumbnail from "../../public/images/josh-soriano-UmNl827g6AE-unsplas.jpg";
+import styles from "./allblogposts.module.css";
+import ArrowIcon from "../_utils/arrowIcon";
+
+export default function AllBlogPosts() {
+  return (
+    <>
+      <section className={styles.blogPostFilters}>
+        <div className={styles.filterContainer}>
+          <label htmlFor="search">Search</label>
+          <input type="text" id="search" placeholder="Search" />
+          <button className={styles.filterConfirmButton}>
+            <ArrowIcon />
+          </button>
+        </div>
+
+        <div className={styles.filterContainer}>
+          <label htmlFor="category">Filter By Categories</label>
+          <input type="text" id="category" placeholder="Filter" />
+          <button className={styles.filterConfirmButton}>
+            <ArrowIcon />
+          </button>
+        </div>
+
+        <div className={styles.filterContainer}>
+          <label htmlFor="email">Get The Latest Updates</label>
+          <input type="text" id="email" placeholder="Email" />
+          <button className={styles.filterConfirmButton}>
+            <ArrowIcon />
+          </button>
+        </div>
+      </section>
+
+      <h3 className={styles.blogPostGridHeadline}>The Latest</h3>
+      <section className={styles.blogPostGrid}>
+        <article className={styles.featuredBlogPost}>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+      </section>
+
+      <h3 className={styles.blogPostGridHeadline}>Recommended</h3>
+      <section className={styles.blogPostGrid}>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+      </section>
+
+      <h3 className={styles.blogPostGridHeadline}>All articles</h3>
+      <section className={styles.blogPostGrid}>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+        <article>
+          <Image
+            className={styles.blogPostThumbnail}
+            src={thumbnail}
+            width={1000}
+            height={800}
+            alt="Thumbnail Image"
+          />
+          <div className={styles.blogPostMeta}>
+            <h4>Blog Post Title is Inserted Here</h4>
+            <h5>Tuesday, March 18, 2009</h5>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import styles from "./page.module.css";
-import Image from "next/image";
-import thumbnail from "../public/images/josh-soriano-UmNl827g6AE-unsplas.jpg";
 
 import Header from "./_components/header";
+import AllBlogPosts from "./_components/allblogposts";
+import Footer from "./_components/footer";
 
 export default function Home() {
   return (
@@ -10,31 +10,10 @@ export default function Home() {
       <Header pageTitle="Blog Section Title" />
       <main className={styles.subpageContainer}>
         <div className="innerContainer">
-          <section>
-            <h3>The Latest</h3>
-            <article className={styles.featured}>
-              <Image
-                src={thumbnail}
-                width={1000}
-                height={800}
-                alt="Thumbnail Image"
-              />
-            </article>
-            <article>
-              <Image
-                src={thumbnail}
-                width={1000}
-                height={800}
-                alt="Thumbnail Image"
-              />
-            </article>
-          </section>
-          <section>
-            <h3>Recommended</h3>
-          </section>
+          <AllBlogPosts />
         </div>
       </main>
-      <footer></footer>
+      <Footer />
     </>
   );
 }
